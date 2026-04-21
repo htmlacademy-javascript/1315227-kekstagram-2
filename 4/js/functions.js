cfd91b51ca81860eac04accc0eb14659ab1f71bb
Keks <keks@htmlacademy.ru>
2026-04-21 08:28:29 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,25 @@
+const checkStringMaxLength = (string = '', maxLength = 0) => string.length <= maxLength;
+
+const checkStringIsPalindrome = (string = '') => {
+  const normalizeString = string.replaceAll(' ', '').toLowerCase();
+  let reverseString = '';
+
+  for (let i = normalizeString.length - 1; i >= 0; i--) {
+    reverseString += normalizeString[i];
+  }
+
+  return reverseString === normalizeString;
+};
+
+const extractNumber = (string = '') => {
+  let stringWithNumbers = '';
+
+  for (let i = 0; i <= string.toString().length; i++) {
+    const number = parseInt(string.toString()[i], 10);
+    if (!Number.isNaN(number)) {
+      stringWithNumbers += number;
+    }
+  }
+
+  return Number(stringWithNumbers) ? Number(stringWithNumbers) : NaN;
+};
